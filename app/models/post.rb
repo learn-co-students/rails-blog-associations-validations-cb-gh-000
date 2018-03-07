e778a49256edd8b6256e8c9e1e5054f1e90de65b
Rauof M <abdelrauof197@gmail.com>
2018-03-07 23:26:54 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
-  
+  validates :content, presence: true
+  validates :name, presence: true
+  belongs_to :user
+  has_many :post_tags
+  has_many :tags, through: :post_tags
 end
